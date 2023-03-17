e4049b99e631d28b2ced1d5ae31d7184e890f13e
unknown <metasuit@ethz.ch>
2023-03-17 14:26:40 +0100

    Added moving average filter

--- generate_sines.py
+++ generate_sines.py
@@ -1,5 +1,6 @@
 import math
 import time
+import random
 
 # Define the number of sine waves and the sampling frequency
 num_waves = 8
@@ -7,6 +8,10 @@ sampling_freq = 30
 
 # Define the output file name
 filename = 'sine_waves.txt'
+
+# Define global parameters for adding artificial noise
+add_noise = True  # Set to True to add noise, False to disable
+noise_amplitude = 0.1  # Amplitude of the noise
 
 # Define the function to write the sine wave data to the file
 def write_sine_data(file):
@@ -20,6 +25,10 @@ def write_sine_data(file):
     while True:
         # Calculate the values of the sine waves
         values = [2*math.sin(i * angle_incr + t) for i in range(num_waves)]
+
+        # Add artificial noise to the values if required
+        if add_noise:
+            values = [v + random.uniform(-noise_amplitude, noise_amplitude) for v in values]
 
         # Write the values to the file
         file.seek(0) # Move the file pointer to the beginning of the file
--- plot_sensing.py
+++ plot_sensing.py
@@ -3,16 +3,29 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter import *
 import threading
 import time
+import numpy as np
+from scipy.signal import medfilt
 
 stop_event = threading.Event()
 BUFFER_SIZE = 20
 buffer = [[] for _ in range(8)]
+window_size = 5 #default window size
 
 # Define a function to read all 8 sine waves from the file
 def read_data():
     with open('sine_waves.txt', 'r') as f:
         wave_data = f.readline().strip().split(',')
     return [float(d) for d in wave_data]
+
+# Define a function to apply the moving average filter to the data
+def moving_average_filter(data, windowsize):
+    #solution convolution
+    """
+    return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
+    """
+    #solution cumsum
+    cumsum_data = np.cumsum(np.insert(data, 0, 0))
+    return (cumsum_data[windowsize:] - cumsum_data[:-windowsize]) / float(windowsize)
 
 
 # Define a function to continuously update the plot with new data
@@ -47,8 +60,12 @@ def update_plot():
 
             # Update the plot with the new data
             for i in range(8):
-                #if len(lines) > i:
-                lines[i].set_data(list(range(len(buffer[i]))), buffer[i])
+                if moving_avg_var.get() == 1 and window_size_entry.get().isdigit():
+                    window_size1 = int(window_size_entry.get())
+                    filtered_data = moving_average_filter(buffer[i], window_size1)
+                    lines[i].set_data(list(range(len(filtered_data))), filtered_data)
+                else:
+                    lines[i].set_data(list(range(len(buffer[i]))), buffer[i])
 
             # Add labels to the x and y axes
             ax.set_xlabel('Time')
@@ -92,6 +109,10 @@ def toggle_save():
 # Create the GUI
 root = Tk()
 
+# Create a frame to contain the Start and Stop buttons, and the new checkbox and entry
+control_frame = Frame(root)
+control_frame.pack(side=TOP, pady=10)
+
 # Create a matplotlib figure and embed it inside the Tkinter window
 fig = plt.figure(figsize=(8, 6), dpi=100)
 ax = fig.add_subplot(111)
@@ -99,16 +120,28 @@ canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
 
 start_button = Button(root, text="Start", command=start_plot)
-start_button.pack()
+start_button.pack(side=LEFT, padx=5)
 
 stop_button = Button(root, text="Stop", command=stop_plot)
-stop_button.pack()
+stop_button.pack(side=LEFT, padx=5)
 
 save_button = Checkbutton(root, text="Save", command=toggle_save)
-save_button.pack()
+save_button.pack(side=LEFT, padx=5)
 
 # Initialize the save flag
 save_flag = False
+
+#Add checkbox and input field for moving average filter
+
+window_size_entry = Entry(root, width=10)
+window_size_entry.pack(side=RIGHT, padx=5)
+window_size_entry.insert(END, "5")
+window_size_label = Label(root, text="Filter window Size:")
+window_size_label.pack(side=RIGHT, padx=5)
+
+moving_avg_var = IntVar()
+moving_avg_checkbox = Checkbutton(root, text="Moving Average", variable=moving_avg_var)
+moving_avg_checkbox.pack(side=RIGHT, padx=5)
 
 # Add labels to the x and y axes
 ax.set_xlabel('Time')
